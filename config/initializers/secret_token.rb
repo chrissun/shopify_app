63fae46e1f591736bff5a64e5e14a543168fb9d5
Christopher Sun <chris.cc.sun@gmail.com>
2013-06-28 11:31:58 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShopifyApp::Application.config.secret_token = '058047cfae3e74bc1296a065b604e5b0c8ac57373e263051aafff5bca0bc5fbc223db4ea87ee9da615fe507920ea263dd9c5b402ff823c470a2d34acedd75f45'
